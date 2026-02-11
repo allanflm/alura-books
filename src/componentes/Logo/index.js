1c1f603353a9a2a7bd01c75b3e35d10f75430d62
Allan <allanfelipedk@gmail.com>
2026-02-11 15:17:22 -0300

    feat: creating the components

---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,19 @@
+import './estilo.css'
+import logo from "../../imagens/logo.svg"
+
+function Logo(){
+    return (
+        <div className='logo'>
+            <img 
+            src={logo} 
+            alt='logo' 
+            className='logo-img'>
+
+            </img>
+
+            <p><strong>AluraBooks</strong></p>
+        </div>
+    )
+}
+
+export default Logo;
